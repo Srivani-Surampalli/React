af62697d748fe446f3bc18d01504800b160495d1
Srivani Surampalli (HCL Technologies Ltd) <v-srsura@microsoft.com>
2019-09-03 12:17:07 +0530

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+//entry --> output
+
+const path =require('path');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output : {
+        path : path.join(__dirname, 'public'),
+        filename :'bundle.js'
+    }
+};
